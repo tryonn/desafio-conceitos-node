18e8e5d19de14dd36d1959ef6455c10c634ff50a
Simão Menezes <simaomenezesneto@gmail.com>
2020-04-07 21:27:47 +0100

    Implementando todoas as rotas.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,72 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { url, title, techs, likes } = request.body;
+
+  const repository = {
+    id: uuid(),
+    url,
+    title,
+    techs,
+    likes
+  }
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const reposIndex = repositories.findIndex(r => r.id = id);
+
+  if (reposIndex < 0){
+    return response.status(400).json({ error: 'Not found ID ' })
+  }
+
+  const repo =  { url, title, techs };
+
+  repositories[reposIndex] = repo;
+
+  return response.json(repo);
+
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const repoIndex = repositories.findIndex( re => re.id = id);
+
+  if (repoIndex < 0){
+    return res.status(400).json({ error: 'Not found ID to delete' })
+  }
+  repositories.splice(repoIndex, 1);
+
+  return res.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repoIndex = repositories.findIndex(r => r.id = id);
+  if (repoIndex < 0){
+    return response.status(400).json({ error: `Repositoy not found with ID: ${id}`});
+  }
+
+  const repo = repositories.find(r => r.id = id);
+
+  repo.likes += 1;
+
+  repositories[repoIndex] = repo;
+  return response.json(repo);
+
 });
 
 module.exports = app;
